9ec069b671b9c07f40f407b53b49e09d91cbc562
D-LL <daniel.lussierlevesque@gmail.com>
2022-03-13 10:43:19 -0400

    link speed to animation

--- Source/SoftDesignTraining/SDTPathFollowingComponent.cpp
+++ Source/SoftDesignTraining/SDTPathFollowingComponent.cpp
@@ -24,6 +24,7 @@ void USDTPathFollowingComponent::FollowPathSegment(float deltaTime)
     else
     {
         Super::FollowPathSegment(deltaTime);
+        Speed = MovementComp->Velocity.Size();
     }
 }
 
